a5ff8b3b78133b92e92c09049c7e92d6d8b26b1f
ojsarver <ojsarver@uncg.edu>
2026-03-03 10:49:11 -0500

    bigpush

--- fungaltime.R
+++ fungaltime.R
@@ -4,11 +4,23 @@ pacman::p_load(tidyverse,
 
 df_fung<-read_csv(here("su25fungv2.csv"))
 
-df_fung%>%
+df_fung2<-df_fung%>%
+  filter(Treat=="C")
+
+df_fung3<-df_fung%>%
+  filter(Treat=="MP")
+
+df_fung2%>%
   ggplot(aes(x= Week,
-             y= Prop,
-             color=Treatment))+
-  geom_line(position='dodge',stat='identity')
+             y= Prop_t,
+             fill=Treatment))+
+  geom_area()
+
+df_fung3%>%
+  ggplot(aes(x= Week,
+             y= Prop_t,
+             fill=Treatment))+
+  geom_area()
 
 df_fung%>%
   ggplot(aes(x= Week,
