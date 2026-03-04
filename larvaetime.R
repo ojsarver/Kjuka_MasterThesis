adcecadbcb71b693539e1be3b1c693e7ebc20319
ojsarver <ojsarver@uncg.edu>
2026-03-03 22:31:13 -0500

    larvdevelopstuff

--- larvaetime.R
+++ larvaetime.R
@@ -10,3 +10,21 @@ df_lar<-read_csv(here("su25larvae1.csv"))
 
 df_larf<-df_lar%>%
   fill(Plant_ID)
+
+df_larf<-df_larf%>%
+  mutate(Adults=as.numeric(Adults))
+
+#how many eggs hatched avg, total, and as a proportion of eggs laid +
+##prop that made it to adult stage from eggs hatched and laid
+
+df_larfc<-df_larf%>%
+  group_by(Treatment) %>%
+  summarize(mu_hat = mean(Eggs_hatched),
+            total_hat=sum(Eggs_hatched),
+            prop_hatched=(sum(Eggs_hatched))/(sum(Eggs_laid)),
+            prop_adult1=(sum(Adults))/(sum(Eggs_hatched)),
+            prop_adult2=(sum(Adults))/(sum(Eggs_laid)))
+
+#zeroes may be an issue?
+
+#Kruskal wallis H test
