6d762d48b829e9786717d002e0d1425222b955c2
ojsarver <ojsarver@uncg.edu>
2026-03-05 14:04:47 -0500

    su25fungalstuff

--- fungaltime.R
+++ fungaltime.R
@@ -5,16 +5,25 @@ pacman::p_load(tidyverse,
 df_fung<-read_csv(here("su25fungv2.csv"))
 df_fung2<-read_csv(here("su25fungv3.csv"))
 
+#fill
+
+df_fungf<-df_fung%>%
+  fill(Week)
+
 #plots
 
-df_fung%>%
+df_fungf%>%
   ggplot(aes(x= Week,
              y= Treatment_sum,
              color=Treatment))+
-  geom_line(position='dodge',stat='identity')
+  geom_line()+
+  labs(x="Week Number",
+       y="Cumulative Plants W/ Damage")
 
 df_fung2%>%
   ggplot(aes(x= Week,
              y= Treatment_sum,
              color=Treatment))+
-  geom_line(position='dodge',stat='identity')
+  geom_line()
+
+            
