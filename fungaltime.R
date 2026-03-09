43f6dc96b05568e1eb6ba3ac5cfb25fad75bb79d
ojsarver <ojsarver@uncg.edu>
2026-03-09 14:49:34 -0400

    su25fungupdate2

--- fungaltime.R
+++ fungaltime.R
@@ -1,7 +1,7 @@
 pacman::p_load(tidyverse,
                patchwork,
                here)
-
+df_fungpa<-read_csv(here("fung_presenceabsence.csv"))
 df_fung<-read_csv(here("su25fungv2.csv"))
 df_fung2<-read_csv(here("su25fungv3.csv"))
 
@@ -26,4 +26,14 @@ df_fung2%>%
              color=Treatment))+
   geom_line()
 
-            
+#stats
+
+fglmm<-glm(Week8~Beetle*Treatment,
+           data=df_fungpa,
+           family=binomial)
+
+Anova(fglmm, type=3)
+
+lsmeans(fglmm, pairwise~Treatment*Beetle, adjust='tukey')
+
+#beetle plants have less fungal presence even after adjusting for observations            
